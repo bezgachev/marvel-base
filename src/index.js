2b011ccc51dd804a9bab43d96215b8e2f3c20396
Ivan Bezgachev <i.bezgachev@yandex.ru>
2024-02-15 19:25:21 +0300

    changes RandomChar, charList, App

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from "react-dom/client";
 import App from './components/app/App';
+// import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
 );
-
