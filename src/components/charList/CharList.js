6c2c56e4bad5ad792c10c2cd701086ecd661313c
Ivan Bezgachev <i.bezgachev@yandex.ru>
2024-02-15 20:48:57 +0300

    загрузить информацию CharInfo по клику персонажа

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -38,6 +38,7 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
+                    onClick={() => this.props.onCharSelected(item.id)}
                     key={item.id}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
